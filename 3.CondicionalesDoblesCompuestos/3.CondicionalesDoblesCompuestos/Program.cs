4ac4cb312928866cce7eb8328d687cc562d58161
Esmuchmocha <dilan.lopez@upb.edu.co>
2026-02-20 13:34:57 -0500

    condicionales dobles
    condicionales dobles

---
+++ 3.CondicionalesDoblesCompuestos/3.CondicionalesDoblesCompuestos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace _3.CondicionalesDoblesCompuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo 
+              supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando
+              que debe abonar impuestos. De lo contrario mostrar el mensaje "persona excenta de pagar 
+              impuestos*/
+            /*string nombre = "";
+            float sueldo = 0;
+            Console.WriteLine("ingrese el nombre del usuario");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese el valor del sueldo");
+            sueldo = Single.Parse(Console.ReadLine());
+
+            if (sueldo >= 3000)
+            {
+                //si la condicion es verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //si la condicion es falsa
+                Console.WriteLine("La persona esta excenta de pagar impuestos");*/
+
+            //*Crear un algoritmo que permita ingresar 2 numeros, si el primer numero es mayor al segundo numero informar su suma y diferencia,
+            //en caso contrario informar el producto y la division del primer umero respecto al segundo.*/
+
+            sbyte num1 = 0;
+            sbyte num2 = 0;
+            Console.WriteLine("ingrese num 1");
+            num1 = Console.ReadLine();
+            Console.WriteLine("ingrese num 2");
+            num2 = Console.ReadLine();
+
+            if (num1>num2)
+            {
+                Console.WriteLine()
+            }
+
+            
+        }
+    }
+}
