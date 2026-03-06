0432b35d39791a19901f6a40c47f36bbbf7e2f56
Esmuchmocha <dilan.lopez@upb.edu.co>
2026-03-06 13:24:01 -0500

    Parcial 1.

---
+++ ParcialNumero1/ParcialNumero1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ParcialNumero1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Cree un algoritmo que le permita a la persona ingresar su nombre y mes de nacimiento,
+             y en base a eso mostrar su signo zodiacal y el elemento que lo representa*/
+
+            string mes = "";
+            string signo = "";
+            string elemento = "";
+
+
+            Console.WriteLine("Ingrese su mes de nacimiento por favor");
+            mes = Console.ReadLine();
+            Console.WriteLine("Ingrese su signo zodiacal por favor");
+            signo = Console.ReadLine();
+
+            if (mes)
+            {
+                //Perdon profe por poder terminar, me quede 45 minutos intentando hacer que esta parte funcionara. Perdon por no poder terminar el parcial.
+                
+            }
+        }
+
+    }
+}
