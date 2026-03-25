fcc395f45e31bd0b0d64a0422d024161bac903d1
Esmuchmocha <dilan.lopez@upb.edu.co>
2026-03-25 13:28:33 -0500

    15.CiclosFor

---
+++ 15.Ciclo_For/15.Ciclo_For/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _15.Ciclo_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int acumulador = 1;
+            for (int contador =1; contador <=5; contador++)
+            {
+                acumulador += contador;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros numeros enteros es {acumulador}");*/
+
+
+
+            /*Realizar un programa que permita obtener la factorial de un numero entero 
+             *ingresado por el teclado*/
+
+            /*decimal acumulador = 1;
+            decimal numero = 0;
+
+            Console.WriteLine("Ingrese un numero por favor");
+            numero = decimal.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador <=numero; contador++) 
+                {
+                acumulador *= contador;
+                Console.WriteLine(acumulador);
+                }
+
+            Console.WriteLine($"El factorial del numero ingresado es {acumulador}");*/
+
+
+            /*Realizar un programa que permita imprimir por pantalla los numeros multiplos de 5,
+             *El usuario debe ingresar por el teclado el rango de numeros a evaluar.*/
+
+            /*int inicioRango;
+            int finalRango;
+
+            Console.WriteLine("Ingrese el inicio del rango:");
+            inicioRango = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el final del rango:");
+            finalRango = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Los numeros multiplos de 5 de {inicioRango} a {finalRango} son:");
+
+            for (int contador = inicioRango; contador <= finalRango; contador++)
+            {
+                if (contador%5 ==0)
+                {
+                    Console.WriteLine(contador);
+                }
+            }*/
+
+            /*En un grupo de 100 alumnos, se desea saber cuantos de los estudiantes son 
+             *hombres y cuantos son mujeres, ademas se desea saber cuantos son mayores 
+             *de edad y cuantos no.*/ 
+
+
+
+
+        } 
+    }
+}
