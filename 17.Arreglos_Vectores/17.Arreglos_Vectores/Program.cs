d7aa2f178f4da65ba1ac63f2a231ca83552c501d
Esmuchmocha <dilan.lopez@upb.edu.co>
2026-04-10 13:30:51 -0500

    17.Arreglos_Vectores

---
+++ 17.Arreglos_Vectores/17.Arreglos_Vectores/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace _17.Arreglos_Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] numeros = new int[5]; //El rango de este valor es 5 
+            numeros[0] = 12;
+            numeros[1] = 13;
+            numeros[2] = 42;
+            numeros[3] = 201;
+            numeros[4] = 0;
+            //numeros[5] = 1; la posicion 6 con indice 5 no existe; porque el rango del vector es 5.
+            Console.WriteLine($"El valor almacenado en la posicion 3, con indice 2 es: {numeros[2]}"); 
+
+            //Recorrer el vector para llenarlo 
+
+            string[] nombres = new string[5];
+
+            for ( int i = 0; i < 5 ; i++ )
+            {
+                Console.WriteLine($"Ingrese el nombre para guardar la posicion {i + 1} con indice {i}:");
+                nombres[i]= Console.ReadLine();
+            }
+
+            char[] simbolos = new char[] { '#', '%', '?', '|', 'Q', 'ñ', '&', '9', '*'};
+
+            //Recorrer vector para recuperar los datos almacenados en el 
+            Console.Clear();
+            for ( int i = 0;i < simbolos.Length;i++ )
+            {
+                Console.Write(simbolos[i] + " | ");
+            }*/
+
+            /*Crear un arreglo llamado "numeros" de 100 elementos, asignar el numero 10 en cada una de
+             *las posiciones del arreglo. Leer el contenido de cada elemento y escribirlo en pantalla.*/
+
+            /*int[] numeros = new int[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+
+             numeros[i] = 10;
+             Console.WriteLine($"Los numeros en el vector son: {numeros[i]}");
+            
+            }*/
+
+            /*LLene un arreglo con 15 numeros ingresados por teclado. Una vez registrado el total de 
+             *valores, muestre en pantallas todos los elementos del arreglo. Finalmente, determine
+             *cual es el numero menor, junto con la posicion que ocupa cada uno dentro del arreglo.*/
+
+            int[] numeros = new int[15];
+          
+
+
+            for (int i = 0; i < 15; i++)
+            {
+                numeros[i] = ;
+
+
+            }
+        }
+    }
+}
