15823ce810ca2381802fbda7751444bb4be70d5e
Esmuchmocha <dilan.lopez@upb.edu.co>
2026-04-15 13:28:45 -0500

    Taller_Vectores.

---
+++ Taller_Vectores/Taller_Vectores/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Taller_Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1-Escriba un algoritmo que permita llenar un vector[15] con numeros enteros;
+             *encuentre y muestre el valor maximo y minimo de los numeros ingresados.*/
+             
+
+            /*int[] numeros = new int[15];
+            int num_mayor = 0;
+            int num_menor = 0;
+
+
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"Ingrese el numero para llenar la posicion {i + 1}, con indice {i}:");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (numeros[i] == 0)
+                {
+                    num_mayor = numeros[i]; num_menor = numeros[i];
+                }
+                else
+                {
+                    if (numeros[i]>num_mayor)
+                    {
+                        num_mayor = numeros[i];
+                    }
+
+                    if (numeros[i]<num_menor)
+                    {
+                       num_menor = numeros[i];
+                    }
+                } 
+
+            }
+
+            Console.WriteLine($"El numero menor es: {num_menor}");
+            Console.WriteLine($"El numero mayor es: {num_mayor}");*/
+
+            /*2-Escriba un algoritmo que permita:
+             *A) Crear 2 vectores del mismo tamaño.
+             *B)LLenarlos con numeros.
+             *C)Comparar posicion con posicion.
+             *D)Indicar cuantos elementos son iguales.*/
+
+        }
+    }
+}
